e543b9f7226dc6c3c4fe623361c7029e5e242172
Kyle Maningo <kikikyleed@gmail.com>
2025-09-29 21:07:46 +0800

    Remove old account app and replace with registration app along with updated skillbridge files

---
+++ registration/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+from registration.forms import CustomUserCreationForm
+
+def index(request):
+    return render(request, 'index.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Account created! Welcome!")
+            return redirect('index')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+
